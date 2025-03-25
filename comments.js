2dbbed76c67c6c9165a40f6a5476c4b5754b3eee
Erleson Sarmento <72279704+erleson-dev@users.noreply.github.com>
2025-03-25 13:24:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,34 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. The comments.html file should be in the same directory as the comments.js file.
+// The comments.html file should have the following content:
+// <!DOCTYPE html>
+// <html>
+// <head>
+//     <title>Comments</title>
+// </head>
+// <body>
+//     <h1>Comments</h1>
+//     <ul id="comments"></ul>
+//     <form id="comment-form">
+//         <input type="text" id="comment" />
+//         <button type="submit">Add Comment</button>
+//     </form>
+//     <script src="comments.js"></script>
+// </body>
+// </html>
+// The web server should serve the comments.html file when a GET request is made to the root URL (/).
+// The web server should serve the comments.html file when a GET request is made to the /comments URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.html URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.htm URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.txt URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.json URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.xml URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.csv URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.rss URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.atom URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.rdf URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.ttl URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.nt URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.n3 URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.trig URL.// The web server should serve the comments.html file when a GET request is made to the /comments.ttl URL.
+// The web server should serve the comments.html file when a GET request is made to the /comments.nt URL.
